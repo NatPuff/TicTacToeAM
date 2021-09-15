c0e44ad4f0b00bf57de934310f28c1b100d8dfe6
Cesar Pacheco <cesarp102104@gmail.com>
2021-09-15 10:14:15 -0500

    animation looking ncie. 180 spin

--- TicTacToeAM/ContentView.swift
+++ TicTacToeAM/ContentView.swift
@@ -35,12 +35,14 @@ struct Home: View{
                     Color.gray
                         .opacity(moves[index] == "" ? 1:0)
                         Text(moves[index])
+                            .font(.system(size: 55))
                             .fontWeight(.heavy)
                             .foregroundColor(.black)
                     }
                         .frame(width: getWidth(), height: getWidth())
                         .cornerRadius(60)
-                    
+                    .rotation3DEffect(.init(degrees: moves[index] != "" ? 180 : 0),
+                                      axis: (x: 0.0, y: 1.0, z: 0.0), anchor: .center, anchorZ: 0.0, perspective: 1.0)
                     
                     .onTapGesture(perform: {
                         withAnimation(Animation.easeIn(duration: 0.5)) {
